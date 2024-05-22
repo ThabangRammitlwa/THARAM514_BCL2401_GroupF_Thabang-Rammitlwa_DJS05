faab5a588111e692a67e0f0aeb34a414fecebea8
Thabang Rammitlwa <thabang451@gmail.com>
2024-05-22 11:41:15 +0200

    adding console.log

--- Redux.js
+++ Redux.js
@@ -1,26 +1,37 @@
-const store=Redux.createStore(reducer)
-let currentState= store.getState()
-let action={
-    type:'LOGIN'
-}
+const initialState= {count:0}
+const reducer=(state=initialState,action)=>{
+    switch(action.type){
+        case 'INCREMENT':
+            console.log('Increment action dispatched')
+            return {...state,count:state.count+1}
+            case 'DECREMENT':
+                console.log('Decrement action dispatched')
+            return {...state,count:state.count-1}
+        default:
+            return state;
+    }
+}  
 
-function actionCreator(){
-    return action;
+//let store = Redux.createStore(counter);
+ const createStore = (reducer)=>{
+    let state = null;
+    const listeners = [];
+ 
+ const getState =()=> state;
+
+ const dispatch = (action)=> {
+    state = reducer(state, action);
+    console.log('New state',state)
+    listeners.forEach(listener=> listener());
+ }
+ const subscribe = (listener)=>{
+    listeners.push(listener);
+    return()=>{
+        listeners = listeners.filter(l=> l !== listener);
+    }
 }
-store.dispatch(loginAction())
-if (action.type==="LOGIN") {
-    return{
-        login:true}
-} else {
-    return State
+dispatch({});
+console.log('initial state',state)
+return {getState,dispatch, subscribe}
 }
-const authReducer=(state=defaultState,action)=>{
-    switch(actiontype){
-        case 'LOGIN':
-            return {
-                authenticated:false
-            }
-            default: return defaultState
-        }
-        
-}
+let store = createStore(reducer)
